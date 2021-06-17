a9c2eeab8b126b8fc099f7a2c474ab8ac1d8a77f
Muneeb <muneeb@bargoventures.com>
2021-06-17 16:36:53 +0500

    add new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Todos from './components/Todos';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div>
         <Header />
+        <Todos />
+        <Footer />
     </div>
   );
 }
